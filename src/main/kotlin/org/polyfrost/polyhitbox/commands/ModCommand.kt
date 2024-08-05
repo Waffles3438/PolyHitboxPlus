a42339ac7e8fde59a510ab3c00c495d5f23b4c23
Waffles <waffles3438@gmail.com>
2024-08-04 21:53:50 -0400

    Update ModCommand.kt

--- src/main/kotlin/org/polyfrost/polyhitbox/commands/ModCommand.kt
+++ src/main/kotlin/org/polyfrost/polyhitbox/commands/ModCommand.kt
@@ -14,17 +14,48 @@ class ModCommand {
 
     @Main
     fun main() {
-        UChat.chat("Friends list:")
+        UChat.chat("§bFriends list:")
         for(player in ModConfig.playerNames) UChat.chat(player)
+    }
+
+    @Main
+    fun main(player: String) {
+        if(player.lowercase() == "clear"){
+            ModConfig.list.clear()
+            ModConfig.playerNames.clear()
+            UChat.chat("Cleared friends list.")
+            return
+        } else if(player.lowercase() == "list"){
+            UChat.chat("§bFriends list:")
+            for(player in ModConfig.playerNames) UChat.chat(player)
+            return
+        }
+        Multithreading.runAsync {
+            try {
+                if(ModConfig.playerNames.contains(player.lowercase())) {
+                    UChat.chat("$player is already in friends list.")
+                } else {
+                    ModConfig.list.add(NetworkUtils.getJsonElement("https://api.mojang.com/users/profiles/minecraft/$player").asJsonObject.get("id").asString)
+                    UChat.chat("Added $player to friends list.")
+                    ModConfig.playerNames.add(player)
+                }
+            } catch (_: Exception) {
+                UChat.chat("Player not found.")
+            }
+        }
     }
 
     @SubCommand
     fun add(player: String) {
         Multithreading.runAsync {
             try {
-                ModConfig.list.add(NetworkUtils.getJsonElement("https://api.mojang.com/users/profiles/minecraft/$player").asJsonObject.get("id").asString)
-                UChat.chat("Added $player to friends list.")
-                ModConfig.playerNames.add(player)
+                if(ModConfig.playerNames.contains(player.lowercase())) {
+                    UChat.chat("$player is already in friends list.")
+                } else {
+                    ModConfig.list.add(NetworkUtils.getJsonElement("https://api.mojang.com/users/profiles/minecraft/$player").asJsonObject.get("id").asString)
+                    UChat.chat("Added $player to friends list.")
+                    ModConfig.playerNames.add(player)
+                }
             } catch (_: Exception) {
                 UChat.chat("Player not found.")
             }
@@ -37,17 +68,10 @@ class ModCommand {
             try {
                 ModConfig.list.remove(NetworkUtils.getJsonElement("https://api.mojang.com/users/profiles/minecraft/$player").asJsonObject.get("id").asString)
                 UChat.chat("Removed $player to friends list.")
-                ModConfig.playerNames.remove(player)
+                ModConfig.playerNames.remove(player.lowercase())
             } catch (_: Exception) {
                 UChat.chat("Player not found.")
             }
         }
-    }
-
-    @SubCommand
-    fun clear(){
-        ModConfig.list.clear()
-        ModConfig.playerNames.clear()
-        UChat.chat("Cleared friends list.")
     }
 }
